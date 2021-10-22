86d75d76ccc9f994b2a5d3b56fd61559ff1b68a3
Jussi <martin.jussi83@hotmail.se>
2021-10-22 10:29:59 +0200

    updated lamp

--- LampConnectWithJava/LampConnectWithJava.ino
+++ LampConnectWithJava/LampConnectWithJava.ino
@@ -1,13 +1,23 @@
-int LED = A0;
+
+int LED1 = 13;
+int LED2 = 12;
+int LED3 = 11;
+int LED4 = 8;
+
 void setup() {
-   pinMode(LED,OUTPUT);
+   pinMode(LED1,OUTPUT);
+  // pinMode(LED2,OUTPUT);
+  // pinMode(LED3,OUTPUT);
+  // pinMode(LED4,OUTPUT);
    Serial.begin(9600); // start serial
+ 
+   
 while (!Serial) {
     ; // wait for serial port to connect.
   }
-}
 
-void loop() {
+}
+void loop(){
  String readString;
   while (Serial.available()) {
     char c = Serial.read();  // reads input from serial 
@@ -16,10 +26,16 @@ void loop() {
   }// depends on the command turn the led on, or off
   if (readString.length() >0) {
     if(readString == "turnOn"){
-    digitalWrite(LED, 1);
+    digitalWrite(LED1,1);
+    //digitalWrite(LED2,0);
+    //digitalWrite(LED3,1);
+    //digitalWrite(LED4,1);
     }
     else if(readString == "turnOff"){
-      digitalWrite(LED, 0);
+      digitalWrite(LED1,0);
+    //digitalWrite(LED2, 1);
+    //digitalWrite(LED3,1);
+    //digitalWrite(LED4,1);
     }
   }
 }
