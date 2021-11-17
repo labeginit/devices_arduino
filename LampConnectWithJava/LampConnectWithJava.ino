a7b7020613bac25654a30bf8f997a939c1c495ad
raghudeepbathula <Raghudeep.bathula98@gmail.com>
2021-11-17 23:30:39 +0100

    added  Indoor led code

--- LampConnectWithJava/LampConnectWithJava.ino
+++ LampConnectWithJava/LampConnectWithJava.ino
@@ -4,16 +4,30 @@ int LED2 = 12;
 int LED3 = 11;
 int LED4 = 8;
 
+int INDORELED1 = 13;
+int INDORELED2 = 12;
+int INDORELED3 = 11;
+int INDORELED4 = 8;
+
 void setup() {
    pinMode(LED1,OUTPUT);
   // pinMode(LED2,OUTPUT);
   // pinMode(LED3,OUTPUT);
   // pinMode(LED4,OUTPUT);
    Serial.begin(9600); // start serial
+
+  pinMode(INDORELED1,OUTPUT);
+  //pinMode(INDORELED2,OUTPUT);
+  //pinMode(INDORELED3,OUTPUT);
+  //pinMode(INDORELED4,OUTPUT);
+  Serial.begin(9600);
+
  
    
 while (!Serial) {
     ; // wait for serial port to connect.
+ while (!Serial){
+    ;
   }
 
 }
@@ -37,5 +51,23 @@ void loop(){
     //digitalWrite(LED3,1);
     //digitalWrite(LED4,1);
     }
+ String readString;
+  while(Serial.available ()){
+    char c = Serial.read();
+    readString +=c;
+    delay(2);
+  }
+  if (readString.length()>0){
+    if(readString=="On"){
+      digitalWrite(INDORELED1,0);
+      //digitalWrite(INDORELED2,0);
+      //digitalWrite(INDORELED3,1);
+      //digitalWrite(INDORELED4,0);
+    }
+    else if (readString =="off"){
+      digitalWrite(INDORELED1,0);
+      //digitalWrite(INDORELED2,1);
+      //digitalWrite(INDORELED3,1);
+      //digitalWrite(INDORELED4,0);
   }
 }
