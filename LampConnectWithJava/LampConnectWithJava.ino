993768bbf303659a5ec63fb802acd0de67f39cd9
Jussi <martin.jussi83@hotmail.se>
2022-01-11 14:34:14 +0100

    final Arduino smartHouse

--- LampConnectWithJava/LampConnectWithJava.ino
+++ LampConnectWithJava/LampConnectWithJava.ino
@@ -1,3 +1,13 @@
+#include <StaticThreadController.h>
+#include <Thread.h>
+#include <ThreadController.h>
+
+ThreadController tc = ThreadController();
+Thread pLoop = Thread();
+Thread vLoop = Thread();
+
+
+
 int LED1 = 13;
 int LED2 = 12;
 int LED3 = 11;
@@ -19,23 +29,36 @@ void setup() {
    pinMode(LED3,OUTPUT);
    pinMode(LED4,OUTPUT);
    Serial.begin(9600); // start serial
+   pLoop.onRun(temp);
+   pLoop.setInterval(300);
    
+     vLoop.onRun(inbrottOn);
+   vLoop.setInterval(300);
+   
+  
+      tc.add(&penisLoop);
+  tc.add(&vaginaLoop);
+   
+
    
 while (!Serial) {
     ; // wait for serial port to connect.
   }
-
+delay(500);
 }
 void loop(){
-  heatingWind();
+  
+ // indoorOff();
+  //lampOn();
+  //lampOff();
+  //heatingWind();
  //heating();
 //heatingWindOff();
-heatingOff();
+//heatingOff();
 //fan();
-  
-//  inbrottOn();
- String readString;
- temp();
+//fanOff();
+
+   String readString;
   while (Serial.available()) {
     char c = Serial.read();  // reads input from serial 
     readString += c; // adds to string 
@@ -47,13 +70,13 @@ heatingOff();
       lampOn();
     }else if(readString == "off"){
      lampOff();
-    
   }else if(readString=="insideOn"){
       indoorOn();
     }else if (readString =="insideOff"){
       indoorOff();
   }else if(readString=="alarmOn"){
-    alarmOn();
+   
+        alarmOn();
   }else if (readString=="alarmOff"){
     alarmoff();
   }else if(readString=="High"){
@@ -64,25 +87,33 @@ heatingOff();
     fanLow();
   }else if (readString=="Off")
   fanOff();
+  }else if (readString=="heaterOn"){
+    heating();
+    heatingWind();     
+  }else if (readString=="heaterOff"){
+    heatingOff();
+    heatingWindOff();
   }
-     
+ 
+
 
 }
 
 
 
 
-   float temp(){
-    int val = 0;
+   void temp(){
+//    int val = 0;
+double val = 0;
     val = analogRead(sensorPin);
   float mv = ( val/1024.0)*5000;
   float cel = mv/10;
-  Serial.print("TEMPRATURE = ");
-  Serial.print(cel);
-  Serial.print("C");
+  //Serial.print("TEMPRATURE = ");
+  int Sator = trunc(cel);
+  Serial.print(Sator);
+//  Serial.print("C");
   Serial.println();
-  delay(1000);
-    delay(3000);
+//   delay(30000);
   }
  void lampOn(){
   digitalWrite(LED1,1);//13
@@ -147,7 +178,7 @@ heatingOff();
   }
 
   void inbrottOn(){
-    int val = 0;
+    int val ;
   val = digitalRead(inbrott);
 
     if(val == 0){
@@ -188,3 +219,4 @@ digitalWrite(LED1,0);//13
     digitalWrite(LED3,0);//11
     digitalWrite(LED4,1);//8
     }
+    
